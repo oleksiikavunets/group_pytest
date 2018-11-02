c3657f8a5c76d770034d798db07f4e7688c5a224
Anton Kobzar <akobzar@luxoft.com>
2018-11-02 12:44:48 +0200

    Added Buttons implementation

---
+++ src/ui/elements/slider_container.py
@@ -0,0 +1,21 @@
+from src.ui.elements.button import Button
+
+
+class SliderContainer(Button):
+    def __init__(self, locator):
+        #TODO: smart XPath :D
+        super().__init__(locator)
+
+    def slide_right(self):
+        pass
+
+    def slide_right(self):
+        pass
+
+    """Real position?"""
+    def get_position(self):
+        pass
+
+    """Percentage of progress bar """
+    def get_value(self):
+        pass
