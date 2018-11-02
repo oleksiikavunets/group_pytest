10ab2cda78cb8df444534d33e08f2c2e4c2784cc
ovpopova <ovpopova@luxoft.com>
2018-11-02 12:14:08 +0200

    app drawer widget added

--- src/ui/widgets/temperature_widget.py
+++ src/ui/widgets/temperature_widget.py
@@ -1,2 +1,8 @@
+from src.ui.elements.button import Button
+
+
 class TemperatureControlls:
-    pass
+    _temperature_btn_locator = "[class='temperature-2DEjG']"
+
+    def __init__(self):
+        self.appDrawer_ = Button(self._settings_tub_locator)
