42874e949c296d57dbdea7f4e9a59d8f26a98534
ovpopova <ovpopova@luxoft.com>
2018-11-02 14:29:25 +0200

    profile and applications area

--- src/ui/widgets/app_drawer/profile/profile_controls.py
+++ src/ui/widgets/app_drawer/profile/profile_controls.py
@@ -4,9 +4,12 @@ from src.ui.elements.button import Button
 class ProfileMenu:
 
     _profile_btn_locator = "[class^='profileContent']"
+    _arrow_button_closed = "[class^='chevronClosed']"
+    _arrow_button_opened = "[class^='chevronOpened']"
     _profile_heading_locator = "[class^='profileHeading']"
     _profile_recomend_area_locator = "[class^='recommendationArea']"
 
     def __init__(self):
         self.profile_btn = Button(self._profile_btn_locator)
-        ##
+        self.arrow_btn = Button(self._arrow_button_closed)
+        
