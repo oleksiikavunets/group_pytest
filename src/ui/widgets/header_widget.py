a7fc632357f351c62e4bdfdcde93ea14be4659b2
ovpopova <ovpopova@luxoft.com>
2018-11-02 11:53:23 +0200

    empty temperature widget added

--- src/ui/widgets/header_widget.py
+++ src/ui/widgets/header_widget.py
@@ -1,6 +1,6 @@
 from src.ui.widgets.app_darwer_widget import AppDrawer
 from src.ui.widgets.settings.controls_widget import Controls
-
+from src.ui.widgets.temperature_widget import TemperatureControlls
 
 
 class Header:
@@ -9,3 +9,6 @@ class Header:
 
     def open_app_drawer(self):
         return AppDrawer()
+
+    def open_temperature_controlls(self):
+        return TemperatureControlls()
