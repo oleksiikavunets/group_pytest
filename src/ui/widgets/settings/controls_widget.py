5fbd3acc9c7cd09eb99a4d56ad6e54a9ee9f3b01
Maksym Hovorun <maksymhovorun@gmail.com>
2018-11-02 15:28:06 +0200

    Fixed lost locators during some merge

--- src/ui/widgets/settings/controls_widget.py
+++ src/ui/widgets/settings/controls_widget.py
@@ -1,12 +1,11 @@
 from selene.support.jquery_style_selectors import s
 
 from src.ui.elements.button import Button
-from src.ui.widgets.base_widget import BaseWidget
 from src.ui.widgets.settings.settings_widget import Settings
 
 
 class Controls:
-
+    _controls_tab_locator = "[id=SettingsDrawerHeader_Controls_Button]"
     _settings_tub_locator = "[id='SettingsDrawerHeader_Settings_Button']"
     _drive_modes_btn_locator = "[id='QuickControls_DriveModes_Button']"
     _driver_assist_btn_locator = "[id='QuickControls_DriverAssist_Button']"
@@ -26,8 +25,8 @@ class Controls:
     _trunk_btn_locator = "[class*='trunk-icon']"
 
     def __init__(self):
-        # super().__init__(self._root_locator)
-        self.settings_tub = Button(self._settings_tub_locator)
+        self.controls_tab = Button(self._controls_tab_locator)
+        self.settings_tab = Button(self._settings_tub_locator)
         self.drive_modes_btn = Button(self._drive_modes_btn_locator)
         self.doors_lock_btn = Button(self._doors_lock_btn_locator)
         self.driver_assist_btn = Button(self._driver_assist_btn_locator)
