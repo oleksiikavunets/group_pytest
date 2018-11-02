5d8a700bcbeecf6d92cefcf434c6820cdd888299
ovpopova <ovpopova@luxoft.com>
2018-11-02 14:51:33 +0200

    tempr removed

--- src/ui/widgets/app_drawer/temperature_controls.py
+++
@@ -1,8 +0,0 @@
-from src.ui.elements.button import Button
-
-
-class TemperatureControlls:
-    _temperature_btn_locator = "[class='temperature-2DEjG']"
-
-    def __init__(self):
-        self.appDrawer_ = Button(self._settings_tub_locator)
